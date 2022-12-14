6194acbf1be94f1829b30865919bdb064a2521a4
Peter Walther <pewa-github@rosasurfer.com>
2022-12-14 15:29:54 +0200

    fix urls

--- web/index.php
+++ web/index.php
@@ -3,6 +3,18 @@ ob_start();
 
 /** @var rosasurfer\Application $app */
 $app = require(__DIR__.'/../app/init.php');
+
+function _getHostname(): string
+{
+    if (!empty($_SERVER['HTTP_HOST'])) {
+        $httpHost = strtolower(trim($_SERVER['HTTP_HOST']));    // nginx doesn't set $_SERVER[SERVER_NAME]
+        if (strlen($httpHost))                                  // automatically to $_SERVER[HTTP_HOST]
+            return $httpHost;
+    }
+    return $_SERVER['SERVER_NAME'];
+}
+
+$host = _getHostname();
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
@@ -18,7 +30,7 @@ EON (index.php)
 <br>
 <br>
 <br>
-<iframe style="clear:both" src="https://local.api.rosasurfer.com/iframe.php"></iframe>
+<iframe style="clear:both" src="https://<?=strStartsWith($host, 'local.') ? 'local.':''?>api.rosasurfer.com/iframe.php"></iframe>
 
 </body>
 </html>
